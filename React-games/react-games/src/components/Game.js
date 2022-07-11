8c90afd510148d09cae2f3979cfa530ded611385
David71700 <dadeire@icloud.com>
2022-07-11 12:51:39 +0200

    Add filterbutton

--- React-games/react-games/src/components/Game.js
+++ React-games/react-games/src/components/Game.js
@@ -1,7 +1,9 @@
+import "./Game.css";
+
 const Game = ({ name, image, note }) => {
   return (
     <div className="cards">
-      <img className="GameImage" src={image} alt={name} />
+      <img className="game_image" src={image} alt={name} />
       <p>{name}</p>
       <p>{note}</p>
     </div>
--- React-games/react-games/src/components/GameList.js
+++ React-games/react-games/src/components/GameList.js
@@ -2,26 +2,34 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Game from "./Game";
 
-const GameList = () => {
-  const [ListGames, setListGames] = useState([]);
-  console.log();
+const GameList = (props) => {
+  const [listGames, setListGames] = useState([]);
+  const [bestGame, setBestGame] = useState([]);
   useEffect(() => {
     axios
       .get("https://apis.wilders.dev/wild-games/games")
       .then((res) => console.log(res.data) || setListGames(res.data));
   }, []);
+
+  function ShowBestGames() {
+    setBestGame(!bestGame);
+  }
+
   return (
     <div>
       <div>Liste des jeux</div>
-      {ListGames.map((game) => (
-        <div key={game.id}>
-          <Game
-            name={game.name}
-            image={game.background_image}
-            note={game.rating}
-          />
-        </div>
-      ))}
+      <button onClick={ShowBestGames}> Meilleur jeux</button>
+      {listGames
+        .filter((listGame) => !bestGame || listGame.rating > 4.5)
+        .map((game) => (
+          <div key={game.id}>
+            <Game
+              name={game.name}
+              image={game.background_image}
+              note={game.rating}
+            />
+          </div>
+        ))}
     </div>
   );
 };
